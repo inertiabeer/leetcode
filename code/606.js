a45f64da5a42b8919edbecc640255ad08231d0f6
inertia <986619667@qq.com>
2018-07-04 10:20:14 +0800

    606转换树为字符串

--- code/606.js
+++ code/606.js
@@ -1,1 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**     "1(2()(4))(3)"
+ * @param {TreeNode} t
+ * @return {string}
+ */
+var tree2str = function (t) {
+    //后序遍历
 
+    var result="";
+    if(t!=null)
+    {
+        result=result+t.val;
+        let lt=tree2str(t.left);
+        let rt=tree2str(t.right);
+        if(lt==""&&rt!="")
+        {
+            result=result+`()(${rt})`
+        }
+        else {
+            lt= (lt==""?"":`(${lt})`);
+            rt=(rt==""?"":`(${rt})`);
+            result+=lt+rt;
+        }
+    }
+    return result;
+
+};
